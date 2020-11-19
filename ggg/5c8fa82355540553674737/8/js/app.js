0373d1a5b1e70e45b36c3b4159d356998ad8297e
PavelKravtsov1313 <kravcov@newsite.by>
2020-11-19 11:02:52 +0300

    first commit work version

---
+++ ggg/5c8fa82355540553674737/8/js/app.js
@@ -0,0 +1,71 @@
+
+    
+function playAllContant(){
+    TweenLite.set("#petals", { perspective:600 })
+    TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+    
+    var total = 50;
+    var warp = document.getElementById("petals"),
+        w = window.innerWidth,
+        h = window.innerHeight;
+    
+    for (i = 0; i < total; i++) {
+        var Div = document.createElement('div');
+        TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+        warp.appendChild(Div);
+        animm(Div);
+    }
+    
+    function animm(elm) {
+        TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
+        TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
+        TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
+    };
+    
+    function R(min,max) { return min+Math.random() * (max-min) };
+    
+    
+    /* https://mattboldt.com/typed.js/ */
+    var typed = new Typed('#text', {
+        strings: ['В день когда ты родилась сами леса Лордерона прошептали твое имя                                                                                                                                     НИКА                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          Никуся мы с гордостью смотрели как ты росла во имя  справедливости.                                                                                                                                                                                                     Не забывай твой род всегда хилил не переагривая.                                                                                                                                       И я уверен что ты проявишь сдержанность будучи наделенной такой властью.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        Лишь захилив рейд ты узнаешь что такое истинная победа!                                                                                                              Знай это ибо когда ты получишь Атиеш                                                                    ты станешь Топ хилом!!'],
+        startDelay: 3000,
+        typeSpeed: 50,
+        backSpeed: 0,
+        fadeOut: true,
+        loop: false,
+        showCursor: false,
+        onComplete: function() {
+            var author = document.getElementById("author");
+            author.style.opacity = 1;
+        }
+    });
+}
+
+
+function visibleContent() {
+    let content = document.querySelector('.content-wrapper');
+    let btn = document.querySelector('.btn');
+        btn.addEventListener('click',function (){
+            content.classList.add('visible');
+            btn.style.display = 'none';
+
+            trigerClick();
+            playAllContant()
+        })
+}
+    visibleContent();
+
+
+    let trigerClick = () => {
+
+        let video = document.querySelector('.video_media');
+        let audio = new Audio();
+      
+        audio.src = 'Arthas!.mp3'; 
+        audio.play();
+        video.play();
+    }
+    
+
+
+
